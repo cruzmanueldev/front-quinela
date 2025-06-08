b6da29bf1ec9056510726da10d3fbacfda125fe4
cmanuelcabbage <cmanuel.cabbage@gmail.com>
2025-06-08 13:02:48 -0500

    0806 Ultimos Resultados Modal

--- src/Components/Home/ModalLastResults.js
+++ src/Components/Home/ModalLastResults.js
@@ -15,11 +15,11 @@ const ModalLastResult = () => {
     loading_data_results
   } = useSelector(({ home }) => home)
 
-  const [ currentJournery, setCurrentJournery ] = useState(15)
+  const [currentJournery, setCurrentJournery] = useState(15)
 
   const dispatch = useDispatch()
 
-  const [ userSelected, setUserSelected ] = useState(null)
+  const [userSelected, setUserSelected] = useState(null)
 
   const closeModal = () => {
     dispatch(ShowModalLastResultsReducer(false))
@@ -40,19 +40,19 @@ const ModalLastResult = () => {
       className='Modal-Form-Quinela'
       closeIcon={false}
     >
-      <div style={{ display: 'flex', justifyContent: 'center', fontWeight:'bold' }}>Ultimos resultados Quinela</div>
-      <div style={{fontWeight:'bold'}}>Jornada: </div>
-      <div style={{display:'flex', gap:'2px', flexWrap:'wrap'}}>
+      <div style={{ display: 'flex', justifyContent: 'center', fontWeight: 'bold' }}>Ultimos resultados Quinela</div>
+      <div style={{ fontWeight: 'bold' }}>Jornada: </div>
+      <div style={{ display: 'flex', gap: '2px', flexWrap: 'wrap' }}>
         {
-          [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15].map( jou => (
+          [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(jou => (
             <Button
               style={{
-                backgroundColor: jou == currentJournery ? '#0958d9':'#FFFFFF',
-                color: jou == currentJournery ? '#FFFFFF':'#0958d9',
+                backgroundColor: jou == currentJournery ? '#0958d9' : '#FFFFFF',
+                color: jou == currentJournery ? '#FFFFFF' : '#0958d9',
                 border: jou == currentJournery ? 'none' : '1px solid #0958d9'
               }}
-              onClick={()=> {
-                if(jou != currentJournery){
+              onClick={() => {
+                if (jou != currentJournery) {
                   getData(jou)
                   setCurrentJournery(jou)
                   setUserSelected(null)
@@ -64,96 +64,115 @@ const ModalLastResult = () => {
       </div>
       {
         rex_data_last_results.length == 0
-        ? <Empty/>
-        : null
+          ? <Empty />
+          : null
       }
 
       {
         loading_data_results == true
-        ? <div style={{display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column'}}>
-              <img
-                  src={ImageLoading}
-                  width={200}
-              />
-              <div style={{fontSize:'20px'}}>Cargando...</div>
+          ? <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+            <img
+              src={ImageLoading}
+              width={200}
+            />
+            <div style={{ fontSize: '20px' }}>Cargando...</div>
           </div>
-        : rex_data_last_results?.map((mat, index) => (
-          // <div>data</div>
-          <Row key={index} gutter={[12, 12]} style={{ display: 'flex', margin: '10px 0' }}>
-            <Col span={24} style={{ display: 'flex', justifyContent: 'space-between', gap: '10px' }}>
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <div>{mat._sum.puupuntostotal}ptos - {mat.usuario.usuusuario}</div>
-              </div>
-              <Button 
-                style={{
-                  backgroundColor: mat.usuid == userSelected ? '#0958d9':'#FFFFFF',
-                  color: mat.usuid == userSelected ? '#FFFFFF':'#0958d9',
-                  border: mat.usuid == userSelected ? 'none' : '1px solid #0958d9'
-                }}
-                onClick={()=>{
-                if(mat.usuid == userSelected){
-                  setUserSelected(null)
-                }else{
-                  setUserSelected(mat.usuid)
-                }
-              }}>Ver partidos</Button>
-            </Col>
-            <Col span={24}>
-              {
-                mat.predicts.filter(usu => mat.usuid == userSelected).map(pre => (
-                  <Row className='ContainerMatchHistorical'>
-                    <Col span={9}>
-                      <div style={{ display: 'flex', justifyContent: 'center', gap: '3px' }}>
-                        <img
-                          height='30'
-                          width='auto'
-                          src={pre.pruparpartidos.parlocalsel.selimagen}
-                        />
-                        <div
-                          style={{
-                            display: 'flex',
-                            alignItems: 'center'
-                          }}
-                        >vs</div>
-                        <img
-                          height='30'
-                          width='auto'
-                          src={pre.pruparpartidos.parvisitasel.selimagen}
-                        />
+          : <>
+            {
+              rex_data_last_results?.map((mat, index) => (
+                <>
+                  <Row key={index} gutter={[12, 12]} style={{ display: 'flex', margin: '10px 0' }}>
+                    <Col span={24} style={{ display: 'flex', justifyContent: 'space-between', gap: '10px' }}>
+                      <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <div>{mat._sum.puupuntostotal}ptos - {mat.usuario.usuusuario}</div>
                       </div>
+                      <Button
+                        style={{
+                          backgroundColor: mat.usuid == userSelected ? '#0958d9' : '#FFFFFF',
+                          color: mat.usuid == userSelected ? '#FFFFFF' : '#0958d9',
+                          border: mat.usuid == userSelected ? 'none' : '1px solid #0958d9'
+                        }}
+                        onClick={() => {
+                          if (mat.usuid == userSelected) {
+                            setUserSelected(null)
+                          } else {
+                            setUserSelected(mat.usuid)
+                          }
+                        }}>Ver partidos</Button>
                     </Col>
-                    <Col span={6} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                      <div style={{ display: 'flex', justifyContent: 'center', gap: '3px', alignItems: 'center', backgroundColor: '#0266E2', borderRadius: '5px', padding: '1px 4px', color: '#FFFFFF' }}>
-                        <div>{pre.pruparpartidos.pargoleslocal}</div>
-                        <div>{'-'}</div>
-                        <div>{pre.pruparpartidos.pargolesvisita}</div>
-                      </div>
-                    </Col>
-                    <Col span={3} style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+                    <Col span={24}>
                       {
-                        pre.prugoleslocal == pre.pruparpartidos.pargoleslocal && pre.prugolesvisita == pre.pruparpartidos.pargolesvisita
-                        ? <CrownOutlined style={{color:'#F0AB42', fontSize:'18px'}}/>
-                        : null
+                        userSelected
+                          ? <Row>
+                            <Col span={9}></Col>
+                            <Col style={{ textAlign: "center", color:'#0958d9' }} span={6}>Resultado</Col>
+                            <Col span={3}></Col>
+                            <Col span={6} style={{ textAlign: "center", color:'#0958d9' }}> Quinela</Col>
+                          </Row>
+                          : null
+                      }
+                      {
+                        mat.predicts.filter(usu => mat.usuid == userSelected).map(pre => (
+                          <>
+                            <Row className='ContainerMatchHistorical'>
+                              <Col span={9}>
+                                <div style={{ display: 'flex', justifyContent: 'center', gap: '3px' }}>
+                                  <img
+                                    height='30'
+                                    width='auto'
+                                    src={pre.pruparpartidos.parlocalsel.selimagen}
+                                  />
+                                  <div
+                                    style={{
+                                      display: 'flex',
+                                      alignItems: 'center'
+                                    }}
+                                  >vs</div>
+                                  <img
+                                    height='30'
+                                    width='auto'
+                                    src={pre.pruparpartidos.parvisitasel.selimagen}
+                                  />
+                                </div>
+                              </Col>
+                              <Col span={6} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                <div style={{ display: 'flex', justifyContent: 'center', gap: '3px', alignItems: 'center', backgroundColor: '#0266E2', borderRadius: '5px', padding: '1px 4px', color: '#FFFFFF' }}>
+                                  <div>{pre.pruparpartidos.pargoleslocal}</div>
+                                  <div>{'-'}</div>
+                                  <div>{pre.pruparpartidos.pargolesvisita}</div>
+                                </div>
+                              </Col>
+                              <Col span={3} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                {
+                                  pre.prugoleslocal == pre.pruparpartidos.pargoleslocal && pre.prugolesvisita == pre.pruparpartidos.pargolesvisita
+                                    ? <CrownOutlined style={{ color: '#F0AB42', fontSize: '18px' }} />
+                                    : null
+                                }
+                              </Col>
+
+                              <Col span={6} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                <div style={{
+                                  display: 'flex', justifyContent: `center`, gap: `3px`, alignItems: `center`,
+                                  backgroundColor: `${pre.parganador == pre.pruganador ? '#237804' : '#ff4d4f'}`,
+                                  borderRadius: `5px`, padding: `1px 4px`, color: `#FFFFFF`
+                                }}
+                                >
+                                  <div style={{ alignItems: 'center', textAlign: 'center' }}>{pre.prugoleslocal}</div>
+                                  <div>{'-'}</div>
+                                  <div>{pre.prugolesvisita}</div>
+                                </div>
+                              </Col>
+                            </Row>
+                          </>
+                        ))
                       }
                     </Col>
-
-                    <Col span={6} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                      <div style={{ display: 'flex', justifyContent: `center`, gap: `3px`, alignItems: `center`, 
-                        backgroundColor: `${pre.parganador == pre.pruganador ? '#237804' : '#ff4d4f'}`, 
-                        borderRadius: `5px`, padding: `1px 4px`, color: `#FFFFFF` }}
-                      >
-                        <div style={{ alignItems: 'center', textAlign: 'center' }}>{pre.prugoleslocal}</div>
-                        <div>{'-'}</div>
-                        <div>{pre.prugolesvisita}</div>
-                      </div>
-                    </Col>
+                    <Divider style={{ marginTop: '2px', marginBottom: '0' }} />
                   </Row>
-                ))
-              }
-            </Col>
-            <Divider style={{marginTop:'2px', marginBottom:'0'}}/>
-          </Row>
-        ))
+                </>
+              ))
+            }
+          </>
       }
     </Modal>
   )
